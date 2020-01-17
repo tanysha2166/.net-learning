9e36e4e839f184c6b5afb2f2102995fbd40f6841
t-vasilevskaya <tanysha2166@gmail.com>
2020-01-17 23:54:54 +0300

    Второе задание

--- Day1/Task2/ConsoleApp4/task2.cs
+++ Day1/Task2/ConsoleApp4/task2.cs
@@ -11,52 +11,49 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             string number;
-            int x1, x2, y1, y2, z1, z2;
             Console.WriteLine("Введите координаты первой точки через запятую");
             number = Console.ReadLine();
-            x1 = GetLeftNumber(number);
-            x2 = GetRightNumber(number);
+            int[] coordinates1 = GetNumbers(number);
             Console.WriteLine("Введите координаты второй точки через запятую");
             number = Console.ReadLine();
-            y1 = GetLeftNumber(number);
-            y2 = GetRightNumber(number);
+            int[] coordinates2 = GetNumbers(number);
             Console.WriteLine("Введите координаты третьей точки через запятую");
             number = Console.ReadLine();
-            z1 = GetLeftNumber(number);
-            z2 = GetRightNumber(number);
-            Console.WriteLine("x1 {0}, x2 {1}, y1 {2}, y2 {3}, z1 {4}, z2 {5}", x1, x2, y1, y2, z1, z2);
-            double S = CountPerimetr(x1, x2, y1, y2, z1, z2);
-            Console.WriteLine("Периметр треугольника равен {0}", S);
+            int[] coordinates3 = GetNumbers(number);
+            double t1 = CountSide(coordinates2[0], coordinates1[0], coordinates2[1], coordinates1[1]);
+            double t2 = CountSide(coordinates3[0], coordinates2[0], coordinates3[1], coordinates2[1]);
+            double t3 = CountSide(coordinates1[0], coordinates3[0], coordinates1[1], coordinates3[1]);
+            double P = (t1 + t2 + t3);
+            double S = CountPloshad (t1, t2, t3);
+            Console.WriteLine ("Стороны треуголинка " +t1 + " " + t2 + " " + t3);
+            Console.WriteLine("Периметр треугольника равен {0}", P);
+            Console.WriteLine("Площадь треугольника равна {0}", S);
             Console.ReadLine();
         }
-        public static double CountPerimetr(int x1, int x2, int y1, int y2, int z1, int z2)
+
+        public static double CountSide(int num1, int num2, int num3, int num4)
         {
-            double t1 = Math.Sqrt(Math.Pow((y1 - x1), 2) + Math.Pow((y2 - x2), 2));
-            double t2 = Math.Sqrt(Math.Pow((z1 - y1), 2) + Math.Pow((z2 - y2), 2));
-            double t3 = Math.Sqrt(Math.Pow((x1 - z1), 2) + Math.Pow((x2 - z2), 2));
+            double t1 = Math.Sqrt(Math.Pow((num1 - num2), 2) + Math.Pow((num3 - num4), 2));
+            return t1;
+        }
+
+        public static double CountPloshad(double t1, double t2, double t3)
+        { 
             double p = (t1 + t2 + t3) / 2;
             double S1 = (p * (p - t1) * (p - t2) * (p - t3));
             double S = Math.Sqrt(S1);
             return S;
         }
-        public static int GetRightNumber(string number)
+        public static int [] GetNumbers(string number)
         {
-            int i = number.IndexOf(",");
-            int l = number.Length;
-            l -= i;    
-            string rightSide = number.Remove(0, (i + 1));           
-            Int32.TryParse(rightSide, out int y2);
-            return y2;
+            string [] numbers = number.Split(',');
+            int[] numbers2 = new int[2];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers2[i] = int.Parse(numbers[i]);
+            }
+            return numbers2;
         }
-        public static int GetLeftNumber(string number)
-        {
-            int i = number.IndexOf(",");
-            int l = number.Length;
-            l -= i;
-            string leftSide = number.Remove(i, l);           
-            Int32.TryParse(leftSide, out int y1);
-            return y1;
-
-        }
+      
     }
 }
